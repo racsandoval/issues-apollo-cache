e2f6274f5e37be52e40372f20752c40ecb1f6613
Rafael Sandoval <rafaelsandoval@rafael.local>
2024-12-08 08:13:13 -0300

    add gitignore

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
